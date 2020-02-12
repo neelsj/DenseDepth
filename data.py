690e2c83af23e729c098a0e997e8d40ad8a857c4
Neel Joshi <neel@microsoft.com>
2020-02-12 11:40:41 -0800

    code to fill zeors for depth iamges as a preprocess, removed from data loading

--- data.py
+++ data.py
@@ -6,8 +6,6 @@ from zipfile import ZipFile
 from keras.utils import Sequence
 from augment import BasicPolicy
 
-from scipy.interpolate import griddata
-
 def extract_zip(input_zip):
     input_zip=ZipFile(input_zip)
     return {name: input_zip.read(name) for name in input_zip.namelist()}
@@ -94,20 +92,11 @@ class NYU_BasicAugmentRGBSequence(Sequence):
             sample = self.dataset[index]
 
             x = np.clip(np.asarray(Image.open( BytesIO(self.data[sample[0]]) )).reshape(480,640,3)/255,0,1)
-            y = np.clip(np.asarray(Image.open( BytesIO(self.data[sample[1]]) )).reshape(480,640,1)/255*self.maxDepth,0,self.maxDepth)
+            y = np.clip(np.asarray(Image.open( BytesIO(self.data[sample[1]]) )).reshape(480,640,1)/255*self.maxDepth,1,self.maxDepth)
             y = DepthNorm(y, maxDepth=self.maxDepth)
 
             batch_x[i] = nyu_resize(x, 480)
-            y = nyu_resize(y, 240)
-
-            ind = y==0
-
-            if (ind.any()):
-                nind = y!=0
-                xx, yy = np.meshgrid(range(320),range(240))
-                y[ind] = griddata((xx[nind], yy[nind]), y[nind], (xx[ind], yy[ind]), method='nearest')
-
-            batch_y[i] = y.reshape(240,320,1)
+            batch_y[i] = nyu_resize(y, 240)
 
             if is_apply_policy: batch_x[i], batch_y[i] = self.policy(batch_x[i], batch_y[i])
 
--- train.py
+++ train.py
@@ -1,4 +1,6 @@
-import os, sys, glob, time, pathlib, argparse
+import os, sys, glob, time, pathlib, argparse, platform, csv, cv2, numpy as np
+from PIL import Image
+from scipy.interpolate import griddata     
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '5'
 
 # Kerasa / TensorFlow
@@ -31,72 +33,116 @@ parser.add_argument('--name', type=str, default='densedepth_nyu', help='A name t
 parser.add_argument('--checkpoint', type=str, default='', help='Start training from an existing model.')
 parser.add_argument('--full', dest='full', action='store_true', help='Full training with metrics, checkpoints, and image samples.')
 
-args = parser.parse_args()
+def hollFillDepthImages(args, r4dl_data_csv_file='r4dl_path_256_data/R4DL_train.csv'):
 
-threads = min(max(args.bs,8), multiprocessing.cpu_count())
+    with open(args.datadir + r4dl_data_csv_file) as data_file:
+        image_data = csv.reader(data_file)
 
-if ('PT_DATA_DIR' in os.environ):
-    data_dir = os.environ['PT_DATA_DIR'] + '/'
-else:
-    data_dir = args.datadir
+        depth_files = []
+        for row in image_data:
+            depth_files.append(row[1])
 
-# Inform about multi-gpu training
-if args.gpus == 1: 
-    os.environ['CUDA_VISIBLE_DEVICES'] = args.gpuids
-    print('Will use GPU ' + args.gpuids)
-else:
-    print('Will use ' + str(args.gpus) + ' GPUs.')
+        depth_files = sorted(set(depth_files))
 
-# Create the model
-model_file = (data_dir + args.checkpoint) if (len(args.checkpoint) != 0) else ''
-model = create_model(existing = model_file)
+        for row in depth_files:
 
-# Data loaders
-if args.data == 'nyu': train_generator, test_generator = get_nyu_train_test_data( args.bs,  data_dir)
-if args.data == 'unreal': train_generator, test_generator = get_unreal_train_test_data( args.bs, data_dir )
-if args.data == 'r4dl': train_generator, test_generator = get_r4dl_train_test_data( args.bs, data_dir, r4dl_data_zipfile=args.datazip, r4dl_data_csv_file=args.datacsv)
+            y = np.asarray(Image.open(args.datadir + row)).copy()
 
-# Training session details
-runID = str(int(time.time())) + '-n' + str(len(train_generator)) + '-e' + str(args.epochs) + '-bs' + str(args.bs) + '-lr' + str(args.lr) + '-' + args.name
-outputPath = data_dir + 'models/'
-runPath = outputPath + runID
-pathlib.Path(runPath).mkdir(parents=True, exist_ok=True)
-print('Output: ' + runPath)
+            ind = y==0
 
- # (optional steps)
-if True:
-    # Keep a copy of this training script and calling arguments
-    with open(__file__, 'r') as training_script: training_script_content = training_script.read()
-    training_script_content = '#' + str(sys.argv) + '\n' + training_script_content
-    with open(runPath+'/'+__file__, 'w') as training_script: training_script.write(training_script_content)
+            if (ind.any()):
 
-    # Generate model plot
-    plot_model(model, to_file=runPath+'/model_plot.svg', show_shapes=True, show_layer_names=True)
+                print(row)
 
-    # Save model summary to file
-    from contextlib import redirect_stdout
-    with open(runPath+'/model_summary.txt', 'w') as f:
-        with redirect_stdout(f): model.summary()
+                #y2 = y.copy()
+                #y2[ind] = 255
+                #y2 = np.concatenate((np.expand_dims(y,2),np.expand_dims(y2,2),np.expand_dims(y,2)),2)
 
-# Multi-gpu setup:
-basemodel = model
-if args.gpus > 1: model = multi_gpu_model(model, gpus=args.gpus)
+                #cv2.imshow("in", y2)
 
-# Optimizer
-optimizer = Adam(lr=args.lr, amsgrad=True)
+                nind = y!=0
+                xx, yy = np.meshgrid(range(y.shape[1]),range(y.shape[0]))
+                y[ind] = griddata((xx[nind], yy[nind]), y[nind], (xx[ind], yy[ind]), method='nearest')
 
-# Compile the model
-print('\n\n\n', 'Compiling model..', runID, '\n\n\tGPU ' + (str(args.gpus)+' gpus' if args.gpus > 1 else args.gpuids)
-        + '\t\tBatch size [ ' + str(args.bs) + ' ] ' + ' \n\n')
-model.compile(loss=depth_loss_function, optimizer=optimizer)
+                #cv2.imshow("out", y)
+                #cv2.waitKey(0)
 
-print('Ready for training using %d GPU and %d CPU threads!\n' % (args.gpus, threads))
+                Image.fromarray(y).save(args.datadir + row)
 
-# Callbacks
-callbacks = get_nyu_callbacks(model, basemodel, train_generator, test_generator, load_test_data(data_dir) if args.full else None , runPath)
+def main(args):
 
-# Start training
-model.fit_generator(train_generator, callbacks=callbacks, validation_data=test_generator, epochs=args.epochs, shuffle=True, workers=threads)
+    threads = min(max(args.bs,8), multiprocessing.cpu_count())
 
-# Save the final trained model:
-basemodel.save(runPath + '/model.h5')
+    # Inform about multi-gpu training
+    if args.gpus == 1: 
+        os.environ['CUDA_VISIBLE_DEVICES'] = args.gpuids
+        print('Will use GPU ' + args.gpuids)
+    else:
+        print('Will use ' + str(args.gpus) + ' GPUs.')
+
+    # Create the model
+    model_file = (args.datadir + args.checkpoint) if (len(args.checkpoint) != 0) else ''
+    model = create_model(existing = model_file)
+
+    # Data loaders
+    if args.data == 'nyu': train_generator, test_generator = get_nyu_train_test_data( args.bs,  args.datadir)
+    if args.data == 'unreal': train_generator, test_generator = get_unreal_train_test_data( args.bs, args.datadir )
+    if args.data == 'r4dl': train_generator, test_generator = get_r4dl_train_test_data( args.bs, args.datadir, r4dl_data_zipfile=args.datazip, r4dl_data_csv_file=args.datacsv)
+
+    # Training session details
+    runID = str(int(time.time())) + '-n' + str(len(train_generator)) + '-e' + str(args.epochs) + '-bs' + str(args.bs) + '-lr' + str(args.lr) + '-' + args.name
+    outputPath = args.datadir + 'models/'
+    runPath = outputPath + runID
+    pathlib.Path(runPath).mkdir(parents=True, exist_ok=True)
+    print('Output: ' + runPath)
+
+     # (optional steps)
+    if True:
+        # Keep a copy of this training script and calling arguments
+        with open(__file__, 'r') as training_script: training_script_content = training_script.read()
+        training_script_content = '#' + str(sys.argv) + '\n' + training_script_content
+        with open(runPath+'/'+os.path.basename(__file__), 'w') as training_script: training_script.write(training_script_content)
+
+        # Generate model plot
+         
+        if(platform.system() != 'Windows'):
+            plot_model(model, to_file=runPath+'/model_plot.svg', show_shapes=True, show_layer_names=True)
+
+        # Save model summary to file
+        from contextlib import redirect_stdout
+        with open(runPath+'/model_summary.txt', 'w') as f:
+            with redirect_stdout(f): model.summary()
+
+    # Multi-gpu setup:
+    basemodel = model
+    if args.gpus > 1: model = multi_gpu_model(model, gpus=args.gpus)
+
+    # Optimizer
+    optimizer = Adam(lr=args.lr, amsgrad=True)
+
+    # Compile the model
+    print('\n\n\n', 'Compiling model..', runID, '\n\n\tGPU ' + (str(args.gpus)+' gpus' if args.gpus > 1 else args.gpuids)
+            + '\t\tBatch size [ ' + str(args.bs) + ' ] ' + ' \n\n')
+    model.compile(loss=depth_loss_function, optimizer=optimizer)
+
+    print('Ready for training using %d GPU and %d CPU threads!\n' % (args.gpus, threads))
+
+    # Callbacks
+    callbacks = get_nyu_callbacks(model, basemodel, train_generator, test_generator, load_test_data(args.datadir) if args.full else None , runPath)
+
+    # Start training
+    model.fit_generator(train_generator, callbacks=callbacks, validation_data=test_generator, epochs=args.epochs, shuffle=True, workers=threads)
+
+    # Save the final trained model:
+    basemodel.save(runPath + '/model.h5')
+
+if __name__ == '__main__':
+
+    args = parser.parse_args()
+
+    if ('PT_DATA_DIR' in os.environ):
+        args.datadir = os.environ['PT_DATA_DIR'] + '/'
+
+    main(args)
+    
+
